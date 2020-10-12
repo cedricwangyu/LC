9ac571c0f0a2026c5fd6b555b4ef23c147679262
Yu Wang <wangyu1@sas.upenn.edu>
2020-10-12 02:38:31 -0400

    update

---
+++ 1081-Smallest_Subsequence_of_Distinct_Characters.py
@@ -0,0 +1,35 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        p = {}
+        for letter in string.ascii_lowercase:
+            p[letter] = None
+            
+        for i,letter in enumerate(s):
+            if p[letter]:
+                p[letter].append(i)
+            else:
+                p[letter] = [i]
+        
+        p = {k:p[k] for k in p if p[k] is not None}
+        res = ''
+        #print(p)
+        while len(p) > 0:
+            M = min([p[letter][-1] for letter in p])
+            for letter in p:
+                if p[letter][0] <= M:
+                    m = p[letter][0]
+                    break
+                    
+            res = res + letter
+            del p[letter]
+            
+            for letter in p:
+                I = next((i for i, x in enumerate(p[letter]) if x > m), None)
+                #p[letter] = [val for val in p[letter] if val > m]
+                p[letter] = p[letter][I:]
+
+            #print(p)
+        
+        #print(res)
+        return res
+        
