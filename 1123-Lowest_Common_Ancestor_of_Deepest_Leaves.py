af8f1d57de1e92d235f959190662188b110e8f56
Yu Wang <wangyu1@sas.upenn.edu>
2020-12-12 16:04:43 -0500

    update

---
+++ 1123-Lowest_Common_Ancestor_of_Deepest_Leaves.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        def helper(node):
+            if node is None: return None, 0
+            leftlev = rightlev = 0
+            left = right = None
+            if node.left: left, leftlev = helper(node.left)
+            if node.right: right, rightlev = helper(node.right)
+            if leftlev > rightlev: return left, leftlev + 1
+            elif leftlev < rightlev: return right, rightlev + 1
+            else: return node, leftlev + 1
+        
+        Node, Lev = helper(root)
+        return Node
