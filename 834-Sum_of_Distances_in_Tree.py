b7ce149760ca4651f93cf6cb84c6c03c2e7033cd
Yu Wang <wangyu1@sas.upenn.edu>
2021-09-04 04:11:42 -0400

    834

---
+++ 834-Sum_of_Distances_in_Tree.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def sumOfDistancesInTree(self, N, edges):
+        graph = collections.defaultdict(set)
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        count = [1] * N
+        ans = [0] * N
+        def dfs(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    dfs(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+
+        def dfs2(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - 2*count[child] + N
+                    dfs2(child, node)
+        dfs()
+        dfs2()
+        return ans
