e3551cb3665ecd82d67d4d93b3dd10f05befb7dd
cedricwangyu <wangyu1@sas.upenn.edu>
2022-12-25 01:13:55 -0500

    Create 2389-Longest_Subsequence_With_Limited_Sum.py

---
+++ 2389-Longest_Subsequence_With_Limited_Sum.py
@@ -0,0 +1,8 @@
+from bisect import bisect_right
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        nums.sort()
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        
+        return [bisect_right(nums, query) for query in queries]
